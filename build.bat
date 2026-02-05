932960422fbb67fa68696019fa059890a29ff0b9
vee anti <113473346+veeeanti@users.noreply.github.com>
2026-02-05 04:58:45 -0600

    add the dependencies and fix the build script, my bad g

--- build.bat
+++ build.bat
@@ -1,22 +1,22 @@
 @echo off
-if exist ".\bin\Release\VeeTaikoCrack.dll" goto:squeakycleenbuild
+if exist ".\bin\Release\net6.0\VeeTaikoCrack.dll" goto:squeekycleenbuild
 echo Restoring...
 dotnet restore
 echo Building release DLL...
 dotnet build --configuration Release
-copy ".\bin\Release\VeeTaikoCrack.dll" ".\VeeTaikoCrack.dll"
+copy ".\bin\Release\net6.0\VeeTaikoCrack.dll" ".\VeeTaikoCrack.dll"
 echo Built plugin can be found here, place it in ".\BepInEx\Plugins" and run Taiko.
 pause>nul
 exit
 
-:squeakycleenbuild
+:squeekycleenbuild
 echo Cleaning first...
 dotnet clean
 echo Restoring...
 dotnet restore
 echo Now building...
 dotnet build --configuration Release
-copy ".\bin\Release\VeeTaikoCrack.dll" ".\VeeTaikoCrack.dll"
+copy ".\bin\Release\net6.0\VeeTaikoCrack.dll" ".\VeeTaikoCrack.dll"
 echo Built plugin can be found here, place it in ".\BepInEx\Plugins" and run Taiko.
 pause>nul
 exit
